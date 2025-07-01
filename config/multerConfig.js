37e38094f34bd09deb92c8343703935f2dfdd0a8
maryamsaye <marynsaye@gmail.com>
2025-07-01 16:49:18 +0100

    changes2

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) cb(null, true);
+  if (file?.mimetype?.startsWith('image/')) cb(null, true);
   else cb(new Error('Only image files are allowed!'), false);
 };
 
