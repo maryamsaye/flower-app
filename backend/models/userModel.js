9969ad917ae6295e4f1889f80d8fa899ac344463
maryamsaye <marynsaye@gmail.com>
2025-07-30 17:48:05 +0100

    aaaa

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,8 +12,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Email is required'],
     unique: true,
-    lowercase: true,
-    trim: true,
+
   },
   password: {
     type: String,
