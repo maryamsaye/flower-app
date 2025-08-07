31f5965c9d5d28dd85a3a3056966a9b27a58d5ca
maryamsaye <marynsaye@gmail.com>
2025-08-07 15:32:44 +0100

    added passwrd

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -37,6 +37,7 @@ const signupUser = async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        password: user.password,
         token,
       },
     });
