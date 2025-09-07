5f5ae22924e94ac5fa3cb96873a3882fd19a0869
Sufag1 <farouqabubakar99@gmail.com>
2025-09-07 14:42:32 +0100

    Backend CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'https://flower-app-frontend.onrender.com',
-    ''
+    'https://flower-frontend-admin.onrender.com'
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
